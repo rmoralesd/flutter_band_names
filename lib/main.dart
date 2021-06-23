ff74f27f337992e54803e632cff7b9f6b4f456ee
Ricardo Morales Domínguez <rmorales@metrofflex.com>
2021-06-22 20:27:18 -0500

    Adding linting

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_band_names/pages/home.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Material App',
+      initialRoute: 'home',
+      routes: {'home': (_) => HomePage()},
+    );
+  }
+}
---
+++ lib/models/band.dart
@@ -0,0 +1,6 @@
+class Band {
+  String id;
+  String name;
+  int votes;
+  Band({this.id, this.name, this.votes});
+}
