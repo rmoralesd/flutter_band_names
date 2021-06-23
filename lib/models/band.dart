8f7f26871add11b0e736ad9ff18be853524558f8
Ricardo Morales Domínguez <rmorales@metrofflex.com>
2021-06-22 20:39:57 -0500

    Model for band

--- lib/models/band.dart
+++ lib/models/band.dart
@@ -3,4 +3,10 @@ class Band {
   String name;
   int votes;
   Band({this.id, this.name, this.votes});
+
+  factory Band.fromMap(Map<String, dynamic> obj) => Band(
+        id: obj['id'].toString(),
+        name: obj['name'].toString(),
+        votes: obj['votes'] as int,
+      );
 }
